d92a5809aaf8b298c9abe6d6ce897ccb342a0a99
Maartje Eyskens <maartje@eyskens.me>
2020-01-02 10:40:35 +0100

    Pull font awesome in via NPM
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- zeephuisje/zeephuisje/src/main.js
+++ zeephuisje/zeephuisje/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Simplert from 'vue2-simplert-plugin'
+
+import '@fortawesome/fontawesome-pro/css/all.css'
+import '@fortawesome/fontawesome-pro/js/all.js'
+
 require('vue2-simplert-plugin/dist/vue2-simplert-plugin.css')
 
 Vue.use(Simplert)
