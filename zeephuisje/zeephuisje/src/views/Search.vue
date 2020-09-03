7ebacbcd4a63841bedcfb2d42895ae29a97e4334
Maartje Eyskens <maartje@eyskens.me>
2020-09-03 13:55:02 +0200

    Empty placeholder
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- zeephuisje/zeephuisje/src/views/Search.vue
+++ zeephuisje/zeephuisje/src/views/Search.vue
@@ -15,7 +15,7 @@
               <div class="input-group-prepend">
                 <div class="input-group-text">{{prefix}}</div>
               </div>
-              <input type="text" class="form-control" v-model="doelgroepnummer" placeholder="1234" v-focus />
+              <input type="text" class="form-control" v-model="doelgroepnummer" placeholder="" v-focus />
             </div>
           </div>
           <div class="co-4">
